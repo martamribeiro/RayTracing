854889ee0a90872624badb10cee71ec099264d19
Marta Ribeiro <63553878+martamribeiro@users.noreply.github.com>
2023-09-18 17:15:35 +0100

    Started developing Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+
+class Program{
+    static void Main(string[] args){
+
+        Console.WriteLine("Insert the name of the scene description file: ");
+        string arquivoDeCena = Console.ReadLine();
+
+        try
+        {
+            // open file
+            using (StreamReader sr = new StreamReader(arquivoDeCena))
+            {
+                string linha;
+                while ((linha = sr.ReadLine()) != null)
+                {
+                    //TODO: process each file line
+                    Console.WriteLine(linha); // write each file line to show the imported file
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"There was an error reading the file: {e.Message}");
+        }
+    }
+}
