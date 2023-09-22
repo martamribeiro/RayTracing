0aea8a64bd857b6352bc5c09af59608ba5b8b54e
Marta Ribeiro <63553878+martamribeiro@users.noreply.github.com>
2023-09-22 22:43:11 +0100

    Developed Vector3 class

--- RayTracingApp/RayTracingApp/Vector3.cs
+++ RayTracingApp/RayTracingApp/Vector3.cs
@@ -8,6 +8,87 @@ namespace RayTracingApp
 {
     internal class Vector3
     {
-        //to develop
+        private float x;
+        private float y;
+        private float z;
+
+        //only read
+        public float X => x;
+        public float Y => y;
+        public float Z => z;
+
+        public Vector3(float x, float y, float z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        //vector operations
+
+        public static Vector3 operator +(Vector3 v1, Vector3 v2)
+        {
+            return new Vector3(v1.X + v2.X, v1.Y + v2.Y, v1.Z + v2.Z);
+        }
+
+        public static Vector3 operator -(Vector3 v1, Vector3 v2)
+        {
+            return new Vector3(v1.X - v2.X, v1.Y - v2.Y, v1.Z - v2.Z);
+        }
+
+        //multiply/divide by scalar
+
+        public static Vector3 operator *(Vector3 v, float scalar)
+        {
+            return new Vector3(v.X * scalar, v.Y * scalar, v.Z * scalar);
+        }
+
+        public static Vector3 operator *(float scalar, Vector3 v)
+        {
+            return v * scalar;
+        }
+
+        public static Vector3 operator /(Vector3 v, float scalar)
+        {
+            if (scalar == 0)
+                throw new ArgumentException("Division by zero is not allowed.");
+
+            return new Vector3(v.X / scalar, v.Y / scalar, v.Z / scalar);
+        }
+
+        //methods
+
+        public float Length()
+        {
+            return (float)Math.Sqrt(x * x + y * y + z * z);
+        }
+
+        public Vector3 Normalize()
+        {
+            float length = Length();
+            if (length == 0)
+                throw new InvalidOperationException("Cannot normalize a zero-length vector.");
+
+            return new Vector3(x / length, y / length, z / length);
+        }
+
+        //scalar product
+
+        public float Dot(Vector3 other)
+        {
+            return x * other.X + y * other.Y + z * other.Z;
+        }
+
+        //vetorial product
+
+        public Vector3 Cross(Vector3 other)
+        {
+            return new Vector3(
+                y * other.Z - z * other.Y,
+                z * other.X - x * other.Z,
+                x * other.Y - y * other.X
+            );
+        }
+
     }
 }
