8052409b5e2714807cd131b3e9ad6f2468faf893
Marta Ribeiro <63553878+martamribeiro@users.noreply.github.com>
2023-09-26 18:22:06 +0100

    Developed Hit and Ray Classes

--- RayTracingApp/RayTracingApp/Hit.cs
+++ RayTracingApp/RayTracingApp/Hit.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,16 @@ namespace RayTracingApp
         private float t;
         //hit color
         private Color3 color;
+        //true only if interseption is found
+        private bool found;
+        //material of intersepted object
+        private Material material;
+        //interception point
+        private Vector3 point;
+        //normal to the plane tangent to the surface of the object at the point of point intersection
+        private Vector3 normal;
+        //min value of distance t found at the moment
+        private float tmin;
 
         //read only
         public float T
@@ -22,11 +33,49 @@ namespace RayTracingApp
         {
             get { return color; }
         }
+        public bool Found
+        {
+            get { return found; }
+        }
+        public Material Material
+        {
+            get { return material; }
+        }
+        public Vector3 Point
+        {
+            get { return point; }
+        }
+        public Vector3 Normal
+        {
+            get { return normal; }
+        }
+        public float Tmin
+        {
+            get { return tmin; }
+        }
 
-        public Hit(float t, Color3 color)
+        public Hit(float t, Color3 color, bool found, Material material, Vector3 point, Vector3 normal, float tmin) : this(t, color)
         {
             this.t = t;
             this.color = color;
+            this.found = found;
+            this.material = material;
+            this.point = point;
+            this.normal = normal;
+            this.tmin = tmin;
+        }
+
+        //segundo slides
+        /*public Hit(float t, Color3 color)
+        {
+            this.t = t;
+            this.color = color;
+        }*/
+
+        //DUVIDA: porquê adicionar o destrutor??
+        ~Hit()
+        {
+
         }
 
         //modifier
@@ -35,9 +84,6 @@ namespace RayTracingApp
             this.t = t;
             this.color = color;
         }
-
-        //DUVIDA: material e normal estao comentados no TR-02 (pag.13) e TR-01 (pag.58)
-        //adiciono ou nao ???
 
     }
 }
--- RayTracingApp/RayTracingApp/Ray.cs
+++ RayTracingApp/RayTracingApp/Ray.cs
@@ -8,6 +8,38 @@ namespace RayTracingApp
 {
     internal class Ray
     {
-        //to develop
+        //ray direction
+        private Vector3 direction;
+        //ray origin
+        private Vector3 origin;
+
+        //read only
+        public Vector3 Direction
+        {
+            get { return direction; }
+        }
+        public Vector3 Origin
+        {
+            get { return origin; }
+        }
+
+        public Ray(Vector3 direction, Vector3 origin)
+        {
+            this.direction = direction;
+            this.origin = origin;
+        }
+
+        //to copy other Ray objects
+        public Ray(Ray other)
+        {
+            direction = new Vector3(other.Direction.X, other.Direction.Y, other.Direction.Z);
+            origin = new Vector3(other.Origin.X, other.Origin.Y, other.Origin.Z);
+        }
+
+        public Vector3 PointAtParameter(float t)
+        {
+            return origin + direction * t;
+        }
+
     }
 }
