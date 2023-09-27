d9e15b9b809c493812e98edeadeaa5e101ab5feb
Sakkris <cristovaogmsampaio0@gmail.com>
2023-09-27 17:08:59 +0100

    Developed the Camera Class.

--- RayTracingApp/RayTracingApp/Camera.cs
+++ RayTracingApp/RayTracingApp/Camera.cs
@@ -8,6 +8,35 @@ namespace RayTracingApp
 {
     internal class Camera
     {
-        //to develop
+        // Transformation applied to the Camera
+        private Transformation transformation;
+
+        private double distance;
+
+        // Field of View
+        private double fov;
+
+        //Getters
+        public Transformation Transformation
+        {
+            get { return this.transformation; }
+        }
+
+        public double Distance
+        {
+            get { return this.distance; }
+        }
+
+        public double Fov
+        {
+            get { return this.fov; }
+        }
+
+        public Camera(Transformation transformation, double distance, double fov)
+        {
+            this.transformation = transformation;
+            this.distance = distance;
+            this.fov = fov;
+        }
     }
 }
