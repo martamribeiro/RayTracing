84f9c7607e1668df72e2f9cbc35d196ce9b4fc49
Marta Ribeiro <63553878+martamribeiro@users.noreply.github.com>
2023-11-05 00:26:47 +0000

    Progress bar added

--- RayTracingApp/RayTracingApp/RayTracer.cs
+++ RayTracingApp/RayTracingApp/RayTracer.cs
@@ -6,6 +6,7 @@ namespace RayTracingApp
     public partial class RayTracer : Form
     {
         private int rec;
+        private Bitmap renderedImage = null;
 
         public RayTracer()
         {
@@ -170,7 +171,7 @@ namespace RayTracingApp
                 return;
 
             //object Graphics to draw on the panel
-            Graphics g = e.Graphics;
+            //Graphics g = e.Graphics;
 
             //raytracer
             double distance = Scene.Instance.Camera.Distance;
@@ -186,76 +187,71 @@ namespace RayTracingApp
             int Hres = Scene.Instance.Image.ResX;
             double width = height * Hres / Vres;
             double s = height / Vres;
+            renderedImage = new Bitmap(Hres, Vres);
 
-            //Hres -> horizontal resolution Vres -> vertical resolution
-            for (int j = 0; j < Vres; j++)
+            progressBar.Minimum = 0;
+            progressBar.Maximum = Vres * Hres;
+
+            using (Graphics g = Graphics.FromImage(renderedImage))
             {
-                for (int i = 0; i < Hres; i++)
+                //Hres -> horizontal resolution Vres -> vertical resolution
+                for (int j = 0; j < Vres; j++)
                 {
-                    //P.x, P.y and P.z for the center of the pixel[i][j]
-                    double P_x = (i + 0.5) * s - width / 2.0;
-                    double P_y = -(j + 0.5) * s + height / 2.0;
-                    double P_z = 0.0; // the projection plane is plane z = 0.0
-                    //direction vector
-                    Vector3 direction = new Vector3((float)P_x, (float)P_y, (float)-distance);
-                    //normalize the direction vector
-                    direction = direction.Normalize();
-                    //construct the ray
-                    Ray ray = new Ray(direction, origin);
+                    for (int i = 0; i < Hres; i++)
+                    {
+                        //P.x, P.y and P.z for the center of the pixel[i][j]
+                        double P_x = (i + 0.5) * s - width / 2.0;
+                        double P_y = -(j + 0.5) * s + height / 2.0;
+                        double P_z = 0.0; // the projection plane is plane z = 0.0
+                                          //direction vector
+                        Vector3 direction = new Vector3((float)P_x, (float)P_y, (float)-distance);
+                        //normalize the direction vector
+                        direction = direction.Normalize();
+                        //construct the ray
+                        Ray ray = new Ray(direction, origin);
 
-                    if (j == 199 - 50 && i == 50)
-                        Debug.Assert(true);
+                        if (j == 199 - 50 && i == 50)
+                            Debug.Assert(true);
 
-                    //call traceRay() function
-                    Color3 color = TraceRay(ray, rec);
-                    //check range R G B need to be between 0 and 1
-                    color.CheckRange();
-                    //convert color format
-                    int red = (int)(255.0 * color.ColR);
-                    int green = (int)(255.0 * color.ColG);
-                    int blue = (int)(255.0 * color.ColB);
-                    Color pixelColor = Color.FromArgb(red, green, blue);
-                    SolidBrush pixelBrush = new SolidBrush(pixelColor);
-                    //draw a 1x1 pixel on panel
-                    g.FillRectangle(pixelBrush, i, j, 1, 1);
+                        //call traceRay() function
+                        Color3 color = TraceRay(ray, rec);
+                        //check range R G B need to be between 0 and 1
+                        color.CheckRange();
+                        //convert color format
+                        int red = (int)(255.0 * color.ColR);
+                        int green = (int)(255.0 * color.ColG);
+                        int blue = (int)(255.0 * color.ColB);
+                        Color pixelColor = Color.FromArgb(red, green, blue);
+                        SolidBrush pixelBrush = new SolidBrush(pixelColor);
+                        //draw a 1x1 pixel on panel
+                        g.FillRectangle(pixelBrush, i, j, 1, 1);
+                        int progress = j * Hres + i;
+                        progressBar.Value = progress;
+                    }
+
                 }
-
             }
+            progressBar.Value = progressBar.Minimum;
+            e.Graphics.DrawImage(renderedImage, 0, 0);
         }
 
         private void saveImageButton_Click(object sender, EventArgs e)
         {
-            if(Scene.Instance.Camera == null)
+            if (renderedImage == null)
             {
                 MessageBox.Show("There is no content to save.", "Warning");
                 return;
             }
 
-            //resolução
-            int Vres = Scene.Instance.Image.ResY;
-            int Hres = Scene.Instance.Image.ResX;
-
-            //bitmap da parte pintada
-            Bitmap bmp = new Bitmap(Hres, Vres);
-
-            //desenhar conteudo no bitmap
-            using (Graphics g = Graphics.FromImage(bmp))
-            {
-                sceneContainer.DrawToBitmap(bmp, new Rectangle(0, 0, Hres, Vres));
-            }
-
-            //guardar o bitmap
+            //Save the already rendered image
             SaveFileDialog saveFileDialog = new SaveFileDialog();
-            saveFileDialog.Filter = "ImageFiles|*.png";
+            saveFileDialog.Filter = "Image Files|*.png";
             saveFileDialog.Title = "Save Image";
             if (saveFileDialog.ShowDialog() == DialogResult.OK)
             {
-                bmp.Save(saveFileDialog.FileName);
+                renderedImage.Save(saveFileDialog.FileName);
                 MessageBox.Show("Image successfully saved.", "Success");
             }
-
-            //limpar o bitmap
-            bmp.Dispose();
         }
     }
 }
